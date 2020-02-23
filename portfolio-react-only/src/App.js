9a516c28be39912c4fc53591be37fef49d890c8d
TheBrellis <brianroyalellis@gmail.com>
2020-02-23 18:53:09 -0500

    setting up imports for App.js

--- portfolio-react-only/src/App.js
+++ portfolio-react-only/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Bio from "./pages/Bio";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
 
 function App() {
   return (

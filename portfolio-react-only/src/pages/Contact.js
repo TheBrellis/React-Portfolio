0b994ad340e2fc4a04adea6a1d3c3aedb9cffb5d
TheBrellis <brianroyalellis@gmail.com>
2020-02-23 19:01:59 -0500

    adding page templates

---
+++ portfolio-react-only/src/pages/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Col, Row, Container, Jumbotron } from "react-bootstrap";
+
+export default function Contact() {
+    return (
+        <div>Contact</div>
+    )
+}
